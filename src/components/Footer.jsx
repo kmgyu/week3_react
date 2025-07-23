0ec3811138dabd929972bd6b1e97e5e2453d265e
kmgyu <lidersy961@gmail.com>
2025-07-23 10:03:54 +0900

    design: tailwindcss 적용

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,10 @@
-    <footer class="footer footer-center p-6 bg-base-200 text-base-content mt-auto shadow-inner">
-      <div>
-        <p>© 2025 Goorm Shop. All rights reserved.</p>
-      </div>
-    </footer>
+export default function Footer() {
+  return (
+  <footer class="footer footer-center p-6 bg-base-200 text-base-content mt-auto shadow-inner">
+    <div>
+      <p>© 2025 Goorm Shop. All rights reserved.</p>
+    </div>
+  </footer>
+  );
+}
+
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
-    <header class="navbar bg-base-100 shadow sticky top-0 z-50">
-      <div class="container mx-auto px-4 flex justify-between items-center w-full">
-        <a href="index.html" class="text-2xl font-bold text-primary">goorm shop</a>
-        <div class="flex gap-4">
-          <a href="subpage.html" class="btn btn-outline btn-sm">서브 페이지로 이동</a>
-          <a href="login.html" class="btn btn-primary btn-sm">로그인</a>
-        </div>
-      </div>
-    </header>
+export default function Header() {
+return (
+<header class="navbar bg-base-100 shadow sticky top-0 z-50">
+  <div class="container mx-auto px-4 flex justify-between items-center w-full">
+    <a href="index.html" class="text-2xl font-bold text-primary">goorm shop</a>
+    <div class="flex gap-4">
+      <a href="subpage.html" class="btn btn-outline btn-sm">서브 페이지로 이동</a>
+      <a href="login.html" class="btn btn-primary btn-sm">로그인</a>
+    </div>
+  </div>
+</header>
+    );
+}
