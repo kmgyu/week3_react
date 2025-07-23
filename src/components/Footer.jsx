fd27916abdcd611149f9e200d6a0ac790542a082
kmgyu <lidersy961@gmail.com>
2025-07-23 09:37:22 +0900

    project init

---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+    <footer class="footer footer-center p-6 bg-base-200 text-base-content mt-auto shadow-inner">
+      <div>
+        <p>© 2025 Goorm Shop. All rights reserved.</p>
+      </div>
+    </footer>
