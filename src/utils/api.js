f5d9d14171e200db97524cebcb54012d268b0b2e
kmgyu <lidersy961@gmail.com>
2025-07-24 14:27:48 +0900

    feat: signup api 추가

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,7 @@
 import axios from 'axios';
 
 const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000'; // Vite 기준
+const loginToken = import.meta.env.VITE_API_LOGIN_TOKEN || '';
 
 /**
  * 상품 리스트 조회
@@ -16,3 +17,26 @@ export async function fetchProducts() {
     throw error;
   }
 }
+
+/**
+ * 회원 가입
+ * @param {*} name 
+ * @param {*} email 
+ * @param {*} password 
+ * @returns 
+ */
+export async function fetchSignUp(name, email, password) {
+    try {
+        const response = await axios.post(`${baseURL}/api/api/signup`, {
+            name: name,
+            email: email,
+            password: password,
+            returnSecureToken: true
+        });
+        return response.data;
+    } catch (error) {
+        console.error('회원가입 중 오류 발생', error);
+        throw error;
+    }
+}
+
